c7fba098690332a091d485ce9855c5989958c0e0
Stephanie Raymos <stephanieraymos@users.noreply.github.com>
2020-09-21 16:15:03 -0700

    Moving files

---
+++ Slider.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Slider extends Component {
+    render() {
+        return (
+            <div>
+              Slider  
+            </div>
+        )
+    }
+}
+
+export default Slider
---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import Slider from './Slider'
+
+function App() {
+  return (
+    <div className="container">
+      <div className="main-image" />
+      <div className="sidebar">
+
+      </div>
+      <Slider />
+    </div>
+  )
+}
+
+export default App;
